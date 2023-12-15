923ed814e16c895dcf070ac432a3f3b659f8afa4
dnaxel <118845082+dnaxel@users.noreply.github.com>
2023-12-15 14:26:31 +0100

    Update main.R

--- main.R
+++ main.R
@@ -8,3 +8,28 @@ library(openxlsx)
 library(ggpubr)
 library(cowplot)
 library(gridExtra)
+
+####################
+### DATA LOADING ###
+####################
+
+################
+### Figure 1 ###
+################
+library(ggmap)
+library(osmdata)
+library(raster)
+
+### Get poligons of all municipalities
+italia_comuni=getData("GADM",country="ITA",level=3)
+emilia_con_comuni=italia_comuni[which(italia_comuni$NAME_1=="Emilia-Romagna"),]
+bird.map.data <- uccelli[uccelli$ANNO!=2012,]
+emilia_con_comuni$NAME_3 <- toupper(emilia_con_comuni$NAME_3)
+#to get ID: emilia_con_comuni@polygons[[1]]@ID
+marche_con_comuni <- italia_comuni[which(italia_comuni$NAME_1=="Marche"),]
+nuovi_comuni <- c("Casteldelci", "Maiolo", "Pennabilli", "Novafeltria", "San Leo", "Sant' Agata Feltria", "Talamello")
+
+### Get Map
+#an API key to download Stadia maps can be obtained at https://client.stadiamaps.com/signup/
+register_stadiamaps(key = "your-API-key")
+map_emilia <- get_map(location = getbb("emilia-romagna"), maptype = "stamen_terrain_lines", source = "stadia")
